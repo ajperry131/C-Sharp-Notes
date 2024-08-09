9348736a073caf4a3c7f92cdfe5fac9a1691413c
Andrew Perry <ajperry131@gmail.com>
2024-08-08 20:10:44 -0400

    Added section for enums and reference versus value

---
+++ Enums/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HelloWorld
+{
+    class Program {
+        static void Main(string[] args) {
+
+            // convert enum to integer
+            var method = ShippingMethod.Express;
+            System.Console.WriteLine((int)method);
+
+            //convert integer to enum
+            var methodId = 3;
+            System.Console.WriteLine((ShippingMethod)methodId);
+
+            //convert enum to string
+            System.Console.WriteLine(method.ToString());
+
+            //convert string to enum
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+            System.Console.WriteLine(shippingMethod);
+        }
+        enum ShippingMethod {
+                RegularAirMail = 1,
+                RegisteredAirMail = 2,
+                Express = 3
+        }
+    }
+}
---
+++ ReferenceAndValueTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HelloWorld
+{
+    class Program {
+        static void Main(string[] args) {
+            // integers are value types
+            var a = 10; // 10
+            var b = a; // 10
+            b++; // a = 10, b = 11
+            Console.WriteLine(string.Format("a: {0}, b: {1}", a, b));
+
+            // arrays are reference types
+            var array1 = new int[3] {1, 2, 3}; // 1, 2, 3
+            var array2 = array1; // 1, 2, 3
+            array2[0] = 0; // array1[0] = 0, array2[0] = 0
+            System.Console.WriteLine(string.Format("array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]));
+
+        }
+    }
+}
